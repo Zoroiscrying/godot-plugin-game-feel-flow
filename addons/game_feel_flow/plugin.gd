94facc7083c4279ed6ddf193c156a8772d56a2ad
Zoroiscrying <zoroiscrying@outlook.com>
2026-06-20 01:53:02 +0800

    feat: add GFF editor plugin with preview window

--- addons/game_feel_flow/plugin.gd
+++ addons/game_feel_flow/plugin.gd
@@ -6,12 +6,25 @@ extends EditorPlugin
 const AUTOLOAD_NAME = "GameFeelFlow"
 const AUTOLOAD_PATH = "res://addons/game_feel_flow/core/game_feel_flow.gd"
 
+var _editor_plugin: EditorPlugin = null
+
 func _enter_tree() -> void:
-    # Add autoload singleton
-    add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
-    print("Game Feel Flow: Plugin enabled")
+	# Add autoload singleton
+	add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
+	
+	# 注册编辑器插件
+	_editor_plugin = preload("res://addons/game_feel_flow/editor/gff_editor_plugin.gd").new()
+	add_child(_editor_plugin)
+	
+	print("Game Feel Flow: Plugin enabled")
 
 func _exit_tree() -> void:
-    # Remove autoload singleton
-    remove_autoload_singleton(AUTOLOAD_NAME)
-    print("Game Feel Flow: Plugin disabled")
+	# Remove autoload singleton
+	remove_autoload_singleton(AUTOLOAD_NAME)
+	
+	# 清理编辑器插件
+	if _editor_plugin:
+		_editor_plugin.queue_free()
+		_editor_plugin = null
+	
+	print("Game Feel Flow: Plugin disabled")
